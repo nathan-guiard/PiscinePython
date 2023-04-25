889f7f2fe82d201f75e879b674c837bf31f3f465
nguiard <nguiard@student.42.fr>
2023-04-25 16:26:55 +0200

    ex 03

--- Module00/ex03/count.py
+++ Module00/ex03/count.py
@@ -1,15 +1,37 @@
 from typing import List
 import sys
+import string
+
+punctuation_mark = [';', '.', ',', ':']
 
 def text_analyzer(text):
 	"""Counting certain characters in a string"""
-	if type(text) == None or type(text) == str:
+	if type(text) == None:
 		text = input("Please provide a string:\n")
 	elif type(text) != str:
 		print("Argument is not a string")
 		return
 	
-	stats: tuple
+	upper = 0
+	lower = 0
+	punc = 0
+	spaces = 0
+
+	for c in text:
+		if c.isupper():
+			upper += 1
+		elif c.islower():
+			lower += 1
+		elif c in string.punctuation:
+			punc += 1
+		elif c.isspace():
+			spaces += 1
+		
+	print("The text contains" + str(len(text)) + "character(s):")
+	print("- " + str(upper) + " upper letter(s)")
+	print("- " + str(lower ) + " lower letter(s)")
+	print("- " + str(punc) + " punctuation mark(s)")
+	print("- " + str(spaces) + " space(s)")
 
 if __name__ == '__main__':
 	if len(sys.argv) == 2:
